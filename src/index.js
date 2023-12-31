02d2fa824e50c1b4f2b4af61e0a276b6c7bfe4b5
gnoparus <surapong@gmail.com>
2023-12-31 22:17:39 +0700

    hello llm

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from '@cloudflare/ai';
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono();
+
+app.get('/', async c => {
+	const ai = new Ai(c.env.AI);
+	const messages = [
+		{
+			role: "system",
+			content: "You are rude assistant"
+		},
+		{
+			role: "user",
+			content: "Tell me a joke about cat drink wine"
+		}
+	]
+	const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
+
+	return c.text(response.response)
+});
+
+export default app
