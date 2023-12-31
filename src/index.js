34900dfc92c18f977fbcefb8f5e3d7f448b7a067
gnoparus <surapong@gmail.com>
2024-01-01 01:02:00 +0700

    Add streaming functionality and templates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { Ai } from '@cloudflare/ai';
 import { Hono } from 'hono';
 
+import template from "./template.html";
+import streamingTemplate from "./template-streaming.html";
+
 const app = new Hono();
 
-app.get('/', async c => {
+app.get("/", (c) => c.html(streamingTemplate));
+app.get("/b", (c) => c.html(template));
+
+app.get('/stream', async c => {
 	const ai = new Ai(c.env.AI);
+
+	const query = c.req.query("query");
+
 	const messages = [
 		{
 			role: "system",
@@ -12,7 +21,25 @@ app.get('/', async c => {
 		},
 		{
 			role: "user",
-			content: "Tell me a joke about cat drink wine"
+			content: query || "Hello"
+		}
+	]
+	const stream = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages, stream: true });
+
+	return new Response(stream, { headers: { "content-type": "text/event-stream" } });
+})
+
+app.post('/', async c => {
+	const ai = new Ai(c.env.AI);
+
+	const messages = [
+		{
+			role: "system",
+			content: "You are rude assistant"
+		},
+		{
+			role: "user",
+			content: body.query || "Hello"
 		}
 	]
 	const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
